ab1e68a6d27b3c03a2ab9ba30ea2b4f76488d4e3
Yuval <yuvala2001@gmail.com>
2026-05-10 23:26:00 +0300

    feat: enhance logging in main.ts with Logger for better application startup feedback

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
@@ -31,10 +32,15 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, document);
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}/api`);
-  console.log(
-    `Swagger documentation is available at: http://localhost:${port}/api/docs`,
-  );
+  await app.listen(port)
+  .then(() => {
+    logger.verbose(`Application is running on: http://localhost:${port}/api`);
+    logger.verbose(
+      `Swagger documentation is available at: http://localhost:${port}/api/docs`,
+    );
+  })
+  .catch((err) => {
+    logger.error('Failed to start application', err);
+  })
 }
 bootstrap();
